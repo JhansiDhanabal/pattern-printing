d5eb8b3aac1f00feeba3d8768ae73331a06ed754
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2024-01-30 20:18:48 +0530

    Create PrintingPattern.java

---
+++ hollow pyramid star/PrintingPattern.java
@@ -0,0 +1,36 @@
+/*
+n = 4
+   *
+  * *
+ *   *
+*******
+
+*/
+package jhansi;
+
+import java.util.Scanner;
+
+public class PrintingPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner o = new Scanner(System.in);
+		System.out.print("n = ");
+		int n = o.nextInt();
+		int k=1;
+		for(int i=0; i<n; i++) {
+			for(int j=n-1;j>i;j--)
+				System.out.print(" ");
+			for(int j=0;j<k;j++) {
+				if(j==0 || j==k-1 || i==n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			k+=2;
+			System.out.println();
+		}
+		o.close();
+	}
+
+}
