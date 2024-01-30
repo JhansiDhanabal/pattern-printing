d378251d2dcc4aeadc6bd4fcd56e5507a5f0e64a
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2024-01-30 19:46:35 +0530

    Create PrintingPattern.java

---
+++ square star pattern/PrintingPattern.java
@@ -0,0 +1,28 @@
+/*
+n = 4
+****
+****
+****
+****
+*/
+package jhansi;
+
+import java.util.Scanner;
+
+public class PrintingPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner o = new Scanner(System.in);
+		System.out.print("n = ");
+		int n = o.nextInt();
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		o.close();
+	}
+
+}
