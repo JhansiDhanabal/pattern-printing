46465260f0f3cc6c126f7f8b0f4e17a583093497
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2024-01-30 20:09:00 +0530

    Create PrintingPattern.java

---
+++ Mirrored Rhombus Pattern/PrintingPattern.java
@@ -0,0 +1,31 @@
+/*
+n = 4
+   ****
+  ****
+ ****
+****
+*/
+package jhansi;
+
+import java.util.Scanner;
+
+public class PrintingPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner o = new Scanner(System.in);
+		System.out.print("n = ");
+		int n = o.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=n-1;j>i;j--)
+				System.out.print(" ");
+			for(int j=0;j<n;j++) 
+				System.out.print("*");
+			
+			System.out.println();
+		}
+		o.close();
+	}
+
+}
