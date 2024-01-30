0c1410abc9c3964dab83ac0c1461f9b7cb1432b5
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2024-01-30 19:50:25 +0530

    Create PrintingPattern.java

---
+++ Hollow sqaure pattern/PrintingPattern.java
@@ -0,0 +1,24 @@
+package jhansi;
+
+import java.util.Scanner;
+
+public class PrintingPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner o = new Scanner(System.in);
+		System.out.print("n = ");
+		int n = o.nextInt();
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n ; j++) {
+				if(i==0 || i==n-1 || j==0 || j==n-1)
+					System.out.print("*");
+				else 
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		o.close();
+	}
+
+}
