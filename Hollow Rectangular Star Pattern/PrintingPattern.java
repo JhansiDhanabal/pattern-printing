ca2f04f481e43967f5b9c937da1875370177f853
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2024-01-30 20:01:00 +0530

    Create PrintingPattern.java

---
+++ Hollow Rectangular Star Pattern/PrintingPattern.java
@@ -0,0 +1,34 @@
+/*
+row and col = 4 6
+******
+*    *
+*    *
+******
+*/
+package jhansi;
+
+import java.util.Scanner;
+
+public class PrintingPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner o = new Scanner(System.in);
+		System.out.print("row and col = ");
+		int row = o.nextInt();
+		int col = o.nextInt();
+		for(int i=0; i<row; i++) {
+			for(int j=0;j<col;j++) {
+				if(i==0 || i==row-1 || j==0 || j==col-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			
+			System.out.println();
+		}
+		o.close();
+	}
+
+}
+
